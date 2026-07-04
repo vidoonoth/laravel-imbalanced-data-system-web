d702541353599ff6fdac96a1ac2282534f71cf2a
vidoonoth <alfinvidoo@gmail.com>
2026-07-05 03:05:34 +0700

    2

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,8 @@
 <tr>
 <td class="header" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; padding: 25px 0; text-align: center;">
-<a href="{{ $url }}" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; color: #2563eb; font-size: 19px; font-weight: bold; text-decoration: none; display: inline-block;">
-{{ $slot }}
+<a href="{{ $url }}" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; color: #2563eb; font-size: 19px; font-weight: bold; text-decoration: none; display: inline-flex; align-items: center; gap: 12px;">
+<img src="{{ asset('images/logo-polindra.png') }}" alt="Logo Polindra" style="width: 40px; height: 40px; object-fit: contain;">
+<span style="color: #1f2937;">{{ $slot }}</span>
 </a>
 </td>
 </tr>
